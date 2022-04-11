8bfde346ad093be4bbf4d872f8c74ded1880a216
soulbird <zhaothreee@gmail.com>
2022-04-11 18:11:02 +0800

    add --allow

--- build-apisix-base-ci.sh
+++ build-apisix-base-ci.sh
@@ -12,12 +12,12 @@ export_openresty_variables() {
     export ld_opt="-L${zlib_prefix}/lib -L${pcre_prefix}/lib -L${openssl_prefix}/lib -Wl,-rpath,${zlib_prefix}/lib:${pcre_prefix}/lib:${openssl_prefix}/lib"
 }
 
-DEBIAN_FRONTEND=noninteractive apt-get update --force-yes
+DEBIAN_FRONTEND=noninteractive apt-get update --allow
 DEBIAN_FRONTEND=noninteractive apt-get install -y sudo git libreadline-dev lsb-release libssl-dev perl build-essential
 DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends wget gnupg ca-certificates
 wget -O - https://openresty.org/package/pubkey.gpg | apt-key add -
 echo "deb http://openresty.org/package/arm64/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/openresty.list
-DEBIAN_FRONTEND=noninteractive apt-get update --force-yes
+DEBIAN_FRONTEND=noninteractive apt-get update --allow
 DEBIAN_FRONTEND=noninteractive apt-get install -y openresty-openssl111-dev openresty-pcre-dev openresty-zlib-dev
 
 export_openresty_variables
