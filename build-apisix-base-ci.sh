0cb0f0db1c2cb6114e940b485313096c511dfaec
soulbird <zhaothreee@gmail.com>
2022-04-11 18:24:50 +0800

    add sudo

--- build-apisix-base-ci.sh
+++ build-apisix-base-ci.sh
@@ -16,7 +16,7 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get update
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y sudo git libreadline-dev lsb-release libssl-dev perl build-essential
 DEBIAN_FRONTEND=noninteractive sudo apt-get -y install --no-install-recommends wget gnupg ca-certificates
 wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
-echo "deb http://openresty.org/package/arm64/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/openresty.list
+echo "deb http://openresty.org/package/arm64/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/openresty.list
 DEBIAN_FRONTEND=noninteractive sudo apt-get update
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y openresty-openssl111-dev openresty-pcre-dev openresty-zlib-dev
 
