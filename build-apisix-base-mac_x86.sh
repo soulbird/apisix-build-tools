4155612b42e1e2cd1e9f2054f63c2902d8c6350a
soulbird <zhaothree@gmail.com>
2022-06-08 14:48:54 +0800

    support build on macos x86

---
+++ build-apisix-base-mac_x86.sh
@@ -0,0 +1,146 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+
+export openssl_prefix=/usr/local/Cellar/openresty-openssl111/1.1.1n_1
+export zlib_prefix=/usr/local/Cellar/zlib/1.2.12
+export pcre_prefix=/usr/local/Cellar/pcre/8.45
+
+export cc_opt="-DNGX_LUA_ABORT_AT_PANIC -I${zlib_prefix}/include -I${pcre_prefix}/include -I${openssl_prefix}/include"
+export ld_opt="-L${zlib_prefix}/lib -L${pcre_prefix}/lib -L${openssl_prefix}/lib -Wl,-rpath,${zlib_prefix}/lib:${pcre_prefix}/lib:${openssl_prefix}/lib"
+
+version=${version:-0.0.0}
+
+if ([ $# -gt 0 ] && [ "$1" == "latest" ]) || [ "$version" == "latest" ]; then
+    ngx_multi_upstream_module_ver=""
+    mod_dubbo_ver=""
+    apisix_nginx_module_ver=""
+    wasm_nginx_module_ver=""
+    lua_var_nginx_module_ver=""
+    debug_args="--with-debug"
+    OR_PREFIX=${OR_PREFIX:="/usr/local/openresty-debug"}
+else
+    ngx_multi_upstream_module_ver="-b 1.0.1"
+    mod_dubbo_ver="-b 1.0.2"
+    apisix_nginx_module_ver="-b 1.8.0"
+    wasm_nginx_module_ver="-b 0.6.0"
+    lua_var_nginx_module_ver="-b v0.5.2"
+    debug_args=${debug_args:-}
+    OR_PREFIX=${OR_PREFIX:="/usr/local/openresty"}
+fi
+
+prev_workdir="$PWD"
+repo=$(basename "$prev_workdir")
+workdir=$(mktemp -d)
+cd "$workdir" || exit 1
+
+or_ver="1.19.9.1"
+wget --no-check-certificate https://openresty.org/download/openresty-${or_ver}.tar.gz
+tar -zxvpf openresty-${or_ver}.tar.gz > /dev/null
+
+if [ "$repo" == ngx_multi_upstream_module ]; then
+    cp -r "$prev_workdir" .
+else
+    git clone --depth=1 $ngx_multi_upstream_module_ver \
+        https://github.com/api7/ngx_multi_upstream_module.git
+fi
+
+if [ "$repo" == mod_dubbo ]; then
+    cp -r "$prev_workdir" .
+else
+    git clone --depth=1 $mod_dubbo_ver \
+        https://github.com/api7/mod_dubbo.git
+fi
+
+if [ "$repo" == apisix-nginx-module ]; then
+    cp -r "$prev_workdir" .
+else
+    git clone --depth=1 $apisix_nginx_module_ver \
+        https://github.com/api7/apisix-nginx-module.git
+fi
+
+if [ "$repo" == wasm-nginx-module ]; then
+    cp -r "$prev_workdir" .
+else
+    git clone --depth=1 -b feat/max_os \
+        https://github.com/soulbird/wasm-nginx-module.git
+fi
+
+if [ "$repo" == lua-var-nginx-module ]; then
+    cp -r "$prev_workdir" .
+else
+    git clone --depth=1 $lua_var_nginx_module_ver \
+        https://github.com/api7/lua-var-nginx-module
+fi
+
+cd ngx_multi_upstream_module || exit 1
+./patch.sh ../openresty-${or_ver}
+cd ..
+
+cd apisix-nginx-module/patch || exit 1
+./patch.sh ../../openresty-${or_ver}
+cd ../..
+
+cd wasm-nginx-module || exit 1
+./install-wasmtime.sh
+cd ..
+
+cc_opt=${cc_opt:-}
+ld_opt=${ld_opt:-}
+luajit_xcflags=${luajit_xcflags:="-DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT"}
+no_pool_patch=${no_pool_patch:-}
+
+cd openresty-${or_ver} || exit 1
+./configure --prefix="$OR_PREFIX" \
+    --with-cc-opt="-DAPISIX_BASE_VER=$version $cc_opt" \
+    --with-ld-opt="-Wl,-rpath,$OR_PREFIX/wasmtime-c-api/lib $ld_opt" \
+    $debug_args \
+    --add-module=../mod_dubbo \
+    --add-module=../ngx_multi_upstream_module \
+    --add-module=../apisix-nginx-module \
+    --add-module=../apisix-nginx-module/src/stream \
+    --add-module=../apisix-nginx-module/src/meta \
+    --add-module=../wasm-nginx-module \
+    --add-module=../lua-var-nginx-module \
+    --with-poll_module \
+    --with-pcre-jit \
+    --without-http_rds_json_module \
+    --without-http_rds_csv_module \
+    --without-lua_rds_parser \
+    --with-stream \
+    --with-stream_ssl_module \
+    --with-stream_ssl_preread_module \
+    --with-http_v2_module \
+    --without-mail_pop3_module \
+    --without-mail_imap_module \
+    --without-mail_smtp_module \
+    --with-http_stub_status_module \
+    --with-http_realip_module \
+    --with-http_addition_module \
+    --with-http_auth_request_module \
+    --with-http_secure_link_module \
+    --with-http_random_index_module \
+    --with-http_gzip_static_module \
+    --with-http_sub_module \
+    --with-http_dav_module \
+    --with-http_flv_module \
+    --with-http_mp4_module \
+    --with-http_gunzip_module \
+    --with-threads \
+    --with-compat \
+    --with-luajit-xcflags="$luajit_xcflags" \
+    $no_pool_patch \
+    -j8
+
+make -j8
+sudo make install
+cd ..
+
+cd apisix-nginx-module || exit 1
+sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
+cd ..
+
+cd wasm-nginx-module || exit 1
+sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
+cd ..
