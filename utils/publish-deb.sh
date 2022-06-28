8fef355462281315a41116e8a00ecfc32444e252
soulbird <zhaothree@gmail.com>
2022-06-28 17:22:21 +0800

    fix ci

--- utils/publish-deb.sh
+++ utils/publish-deb.sh
@@ -12,7 +12,7 @@ env
 VAR_TENCENT_COS_UTILS_VERSION=${VAR_TENCENT_COS_UTILS_VERSION:-v0.11.0-beta}
 VAR_RPM_WORKBENCH_DIR=${VAR_RPM_WORKBENCH_DIR:-/tmp/output}
 VAR_GPG_PRIV_KET=${VAR_GPG_PRIV_KET:-/tmp/dep-gpg-publish.private}
-VAR_GPG_PASSPHRASE=${VAR_GPG_PASSPHRASE:-/tmp/dep-gpg-publish.passphrase}
+VAR_GPG_PASSPHRASE=${VAR_GPG_PASSPHRASE:-/tmp/deb-gpg-publish.passphrase}
 
 
 func_gpg_key_load() {
